6295abe56062ced121c21f1ba97f3219f62435ca
22127294 <hpnghia22@clc.fitus.edu.vn>
2025-05-29 20:28:50 +0700

    Init

---
+++ auth-service/index.js
@@ -0,0 +1,45 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool({
+    user: process.env.DB_USER || "postgres",
+    host: process.env.DB_HOST || "localhost",
+    database: process.env.DB_NAME || "auth_db",
+    password: process.env.DB_PASS || "password",
+    port: "5432" || process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+app.get('/test-db', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM public.users');
+        //print the result to console for debugging
+        res.json({ success: true, data: result.rows });
+    } catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+
+    if (user.rows.length && user.rows[0].password === password) {
+        res.json({ message: 'Login success', user: user.rows[0] });
+    } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+    }
+});
+
+app.listen(4000, () => console.log('Auth service on port 4000'));
